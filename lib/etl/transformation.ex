ddaf41ec5b26087bf50b27b25ad6a90ec19971e2
Brian Balser <brian.balser@gmail.com>
2020-06-15 16:10:56 -0400

    Added base impls for transformation as a function and as a stage

--- lib/etl/transformation.ex
+++ lib/etl/transformation.ex
@@ -11,3 +11,43 @@ defprotocol Etl.Transformation do
   @spec dictionary(Etl.Dictionary.t()) :: {:ok, Etl.Dictionary.t()} | {:error, reason :: term()}
   def dictionary(dictionary)
 end
+
+defmodule Etl.Transformation.Stage do
+  defmacro __using__(_opts) do
+    quote do
+      def stage_or_function(_t) do
+        :stage
+      end
+
+      def function(_t, _context) do
+        fn x -> x end
+      end
+
+      def dictionary(dictionary) do
+        {:ok, dictionary}
+      end
+
+      defoverridable dictionary: 1
+    end
+  end
+end
+
+defmodule Etl.Transformation.Function do
+  defmacro __using__(_opts) do
+    quote do
+      def stage_or_function(_t) do
+        :function
+      end
+
+      def stages(_t, _context) do
+        []
+      end
+
+      def dictionary(dictionary) do
+        {:ok, dictionary}
+      end
+
+      defoverridable dictionary: 1
+    end
+  end
+end
