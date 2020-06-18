090cf94128f415039b975fbb39743ce998adee50
jeffgrunewald <jeff@grunewalddesign.com>
2020-06-18 13:49:19 -0400

    Admin stuff

--- mix.exs
+++ mix.exs
@@ -1,6 +1,8 @@
 defmodule Etl.MixProject do
   use Mix.Project
 
+  @github "https://github.com/inhindsight/etl"
+
   def project do
     [
       app: :etl,
@@ -8,7 +10,13 @@ defmodule Etl.MixProject do
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      description: description(),
+      package: package(),
+      source_url: @github,
+      homepage: @github,
+      docs: docs(),
       elixirc_paths: elixirc_paths(Mix.env()),
+      dialyzer: [plt_file: {:no_warn, ".dialyzer/#{System.version()}.plt"}]
     ]
   end
 
@@ -25,6 +33,7 @@ defmodule Etl.MixProject do
       {:brex_result, "~> 0.4.0"},
       {:timex, "~> 3.6"},
       {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.22.1", only: [:dev]},
       {:placebo, "~> 1.2", only: [:dev, :test]},
       {:checkov, "~> 1.0", only: [:dev, :test]}
     ]
@@ -32,4 +41,24 @@ defmodule Etl.MixProject do
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
+
+  defp package do
+    [
+      maintainers: ["Brian Balser", "Johnson Denen", "Jeff Grunewald"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => @github}
+    ]
+  end
+
+  defp docs do
+    [
+      source_url: @github,
+      extras: ["README.md"],
+      source_url_pattern: "#{@github}/blob/master/%{path}#L%{line}"
+    ]
+  end
+
+  defp description do
+    "Declarative definition of Extract/Transform/Load pipelines"
+  end
 end
